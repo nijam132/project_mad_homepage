03aa1d7ab9189b6772212156b0eca1f327eb65f8
nijam132 <118953034+nijam132@users.noreply.github.com>
2024-01-14 02:22:17 +0800

    Initial commit

---
+++ app/src/main/java/com/example/project_mad_homepage/IntroActivity.java
@@ -0,0 +1,29 @@
+package com.example.project_mad_homepage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        ImageView myImageView = findViewById(R.id.next);
+
+        // Set an OnClickListener for the ImageView
+        myImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
